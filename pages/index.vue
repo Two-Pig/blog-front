0987018eae24aa51918e3974f836d57c581dbd9d
yls <yanglusheng@gildata.com>
2020-11-24 09:59:57 +0800

    'container'

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,40 @@
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  .center {
+    display: flex;
+    justify-content: space-between;
+    background-color: pink;
+    width: 60%;
+    margin: 10px auto;
+    .blog-list {
+      flex: 7;
+      padding: 14px;
+      background-color: red;
+
+      .blog-list-header {
+        border: 1px solid black;
+      }
+    }
+    .categoryt {
+      flex: 3;
+      padding: 14px;
+      background-color: green;
+    }
+  }
+}
+</style>
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">blog-front</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <span>{{ name }}</span>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-        <ul>
-          
-        </ul>
+    <div class="center">
+      <div class="blog-list">
+        <div class="blog-list-header">列表头</div>
       </div>
+      <div class="categoryt">标签分类</div>
     </div>
   </div>
 </template>
@@ -53,35 +63,3 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
